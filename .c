d5202a7b39ac419c9ee22fcdf16ef3dc4fd84978
RudraCodeUp <141010176+RudraCodeUp@users.noreply.github.com>
2024-02-22 10:56:19 +0530

    Update .c

--- .c
+++ .c
@@ -2,16 +2,8 @@
 int main()
 {
 	int days,month,year;
-	printf("enter the month:");
-	scanf("%d",&month);
-	switch(month){
-		case 1:case 3:case 5: case 7:case 9:case 11:
-			printf("31 days");
-			break;
-		case 4:case 6:case 8: case 10: case 12:
-			printf("30 days");
-			break;
-		case 2:
+	
+		
 			printf("enter the year");
 			scanf("%d",&year);
 			if ((year % 100==0) && (year % 400==0))
